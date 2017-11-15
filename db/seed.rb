b5bdfed8ec8623403d9ac9f0a7337a10fb868a1f
pawlkris <paulkristapovich@gmail.com>
2017-11-15 11:30:23 -0500

    added main seeds (still need join object seeds).

--- db/seed.rb
+++ db/seed.rb
@@ -3,10 +3,13 @@ url = "https://data.ny.gov/resource/vn2g-zyb7.json"
 page_data = HTTParty.get(url)
 responses = page_data.parsed_response
 
-counties = responses.collect{|x| x["county"]}.uniq
+counties = responses.collect{|x| x["county"].downcase}.uniq
 # when done go back and look for slashes, which indicate 2 counties (e.g. "Broome/Delaware")
 counties.each{|x| County.create(x})}
 # makes all County instances
+
+waterbodies = responses.collect{|x| x["water"].downcase}.uniq
+waterbodies.each{|x| Waterbody.create(x})}
 
 fishes_dirty = responses.collect{|x| x["fish_speci"]}
 # pulls all fishes from responses
@@ -14,5 +17,5 @@ fishes_clean = fishes_dirty.join("-").split("-").collect{|x|x.strip.downcase}.un
 # fish_speci from api is currently separated by hyphens. we join all response entires
 # and then split by hypen to get array of all fishes in all lakes, then remove whitespace
 # and run uniq to get unique list.
-fishes_clean.each{|x| County.create(x})}
+fishes_clean.each{|x| Fish.create(x})}
 # makes all fish instances
