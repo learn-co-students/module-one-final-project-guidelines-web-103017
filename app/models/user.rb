c29c08143c4efd599f52f11ae89329cfb78be812
chris <christofer.d.johnson@gmail.com>
2017-11-16 18:11:08 -0500

    Added run; Refactoring needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,33 @@
 require 'json'
 require 'rest-client'
 
+
 class User < ActiveRecord::Base
 	has_many :useringredients
 	has_many :ingredients, through: :useringredients
 	has_many :userrecipes
 	has_many :recipes, through: :userrecipes
+	
+	# def initialize
+	# 	@missing_ingredients = 2
+	# 	self.all_cocktails = nil
+	# end
+
+	def missing_ingredients
+		@missing_ingredients
+	end
+
+	def missing_ingredients=(missing_ingredients)
+		@missing_ingredients = missing_ingredients
+	end
+
+	def all_cocktails
+		@all_cocktails
+	end
+
+	def all_cocktails=(all_cocktails)
+		@all_cocktails = all_cocktails
+	end
 
 	def add_ingredients(array)
 		array.each do |ingredient|
@@ -14,6 +36,7 @@ class User < ActiveRecord::Base
 			new_ingredient.save
 		 	self.ingredients << new_ingredient
 		end
+		puts "Ingredients Added!"
 	end
 
 	def findrecipes
@@ -42,11 +65,11 @@ class User < ActiveRecord::Base
 			recipe_hash[ingredient] = recipe_hash[ingredient]['drinks'].map{|x| x['idDrink']}
 		end
 
-		all_cocktails = Hash.new{|hash,key| hash[key]=[]}
+		self.all_cocktails = Hash.new{|hash,key| hash[key]=[]}
 
 		recipe_hash.each do |ingredient,v|
 			recipe_hash[ingredient].each do |recipe_id|
-				all_cocktails[recipe_id] << ingredient
+				self.all_cocktails[recipe_id] << ingredient
 			end
 		end
 
@@ -72,32 +95,30 @@ class User < ActiveRecord::Base
 		# recipes.each{|k,v| v.compact!}
 
 		# recipes.each do |ingredients,recipe_names|
-		all_cocktails.each do |id, ingredients|
+		self.all_cocktails.each do |id, ingredients|
 			response = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}")
 			cocktail_ing = JSON.parse(response)
 			ingredient = cocktail_ing['drinks'][0].map{|ids,ingredient| ingredient.downcase if ingredient && ids.include?("strIngredient")}.compact!
 			ingredient.reject!(&:empty?)
-			all_cocktails[id] << ingredient
-			all_cocktails[id].flatten!.uniq!
+			self.all_cocktails[id] << ingredient
+			self.all_cocktails[id].flatten!.uniq!
 			#binding.pry
 		end
-
-		                 
-		 all_cocktails = Hash[all_cocktails.sort_by do |k,v|
+	                 
+		 self.all_cocktails = Hash[self.all_cocktails.sort_by do |k,v|
 		 	duplicate = v 
 		     arr = duplicate - ingredients.to_a 
 		     arr.length
 		   end  
 		 ] 
 
-		 # missing_ingredients =Hash[all_cocktails.sort_by do |k,v|
+		 # self.missing_ingredients = Hash[self.all_cocktails.sort_by do |k,v|
 		 # 	arr = v&ingredients.to_a
 		 # 	arr.each{|el| v.delete(el)}
 		 #   end  
 		 # ]
-		 #binding.pry
 
-		 all_cocktails.each do |recipe_id,ingredients|
+		 self.all_cocktails.each do |recipe_id,ingredients|
 		 	recipe = Recipe.find_or_create_by(name: recipe_id)
 		 	ingredients.each do |recipe_ingredient|
 		 		recipe.ingredients << Ingredient.find_or_create_by(name: recipe_ingredient)
@@ -106,10 +127,52 @@ class User < ActiveRecord::Base
 		 	self.recipes << recipe
 		 end
 
+		self.all_cocktails
+	end
 
+	def select_recipe(recipe_id)
+
+		response = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{recipe_id}")
+		details = JSON.parse(response)
+
+		puts "Name: #{details['drinks'][0]['strDrink']} (#{details['drinks'][0]['strAlcoholic']})"
+
+		ingredients = details['drinks'][0].map{|ids,ingredient| ingredient.downcase if ingredient && ids.include?("strIngredient")}.compact!
+		ingredients.reject!(&:empty?)
+
+		measurements = details['drinks'][0].map{|ids,ingredient| ingredient.downcase if ingredient && ids.include?("strMeasure")}.compact!
+		measurements.reject!(&:empty?)
+
+		ingredient_measure = Hash[ingredients.zip(measurements)]
+
+		puts "Ingredients: "
+		ingredient_measure.each{|k,v| puts k +' - ' +v}
+		puts "Should be served in: #{details['drinks'][0]['strGlass']}"
+		puts "Instuctions: #{details['drinks'][0]['strInstructions']}"
+		puts "You appear to be missing: "
+		puts self.missing_ingredients[recipe_id]
+		# puts "Would like to find these ingredients at your Wal-mart®?"
+		# input = gets.chomp
+		# if input.downcase=='yes' || input.downcase=='y'
+		# 	puts "We need your Location. Input you 5 digit ZIP-CODE: "
+		# 	input = gets.chomp
+		# 	input.to_i
+		# 	while input < 5 || input > 5
+		# 		puts "Incorrect :( ZIP-CODE please try again: "
+		# 		input = gets.chomp
+		# 		input.to_i
+		# 	end
+		# 	self.location = input
+		# 	find_at_walmart(self.missing_ingredients[recipe_id])
+		# end
 
 	end
 
+	# def find_at_walmart([array])
+	# 	array.each do |ingredient|
 
+
+	# 	end
+	# end
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,79 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
 
+# printf '   *                              
+#  (  `                             
+#  )\))(   (      )      (          
+# ((_)()\  )\  ( /(      )\  `  )   
+# (_()((_)((_) )\())  _ ((_) /(/(   
+# |  \/  | (_)((_)\  | | | |((_)_\  
+# | |\/| | | |\ \ /  | |_| || '_ \) 
+# |_|  |_| |_|/_\_\   \___/ | .__/  
+#                           |_| '
+
+class NewIn
+
+	def welcome
+		puts "Hello Welcome to MixUp. To Begin enter your name: "
+		input = gets.chomp
+		user = User.new(name: input.downcase)
+
+		puts "Hello #{user.name} start by adding ingredients separated by comma:"
+		input = gets.chomp
+		array = input.downcase.split(', ').reject(&:empty?)
+
+		user.add_ingredients(array)
+
+		puts "You can now find recipes by your Ingredients with the command 'find'." 
+		puts "To select a recipe type 'select' followed by your desired recipe name." 
+		puts "To add MOAR Ingredients type 'MOAR'" 
+		puts "And type 'Q' at any time to Quit" 
+
+		while input != "q"
+			input = gets.chomp
+			input.downcase
+
+			case input
+				
+			when "find"
+				puts "This may take a moment. Stay sober...."
+				user.findrecipes
+				puts "To select a recipe type 'select' followed by your desired recipe name." 
+				puts "To add MOAR Ingredients type 'MOAR'" 
+				puts "And type 'Q' at any time to Quit"
+
+			when "select"
+				puts "Enter the recipe name: "
+				userinput = gets.chomp
+				userinput.to_i
+				user.select_recipe(userinput)
+				puts "You can now find recipes by your Ingredients with the command 'find'." 
+				puts "To add MOAR Ingredients type 'MOAR'" 
+				puts "And type 'Q' at any time to Quit"
+
+			when "MOAR"
+				
+				puts "To Begin Add Ingredients separated by comma:"
+				input = gets.chomp
+				array = input.downcase.split(',').reject(&:empty?)
+
+				user.add_ingredients(array)
+				puts "You can now find recipes by your Ingredients with the command 'find'." 
+				puts "To select a recipe type 'select' followed by your desired recipe name." 
+				puts "And type 'Q' at any time to Quit" 
+			else
+				"Invalid input"
+			end
+		end
+	end
+end
+
+newu = NewIn.new
+newu.welcome
+
+	# puts "Welcome #{user}"
+	# puts "To Quit just type 'Q'"
+	# puts "To Begin Add Ingredients separated by comma:"
+	# puts "For Example: Lemon, vodka, ChERy.,etc"
+	# input = gets.chomp
+	# if input == 'Q'
+	# 	puts "Thank You"
