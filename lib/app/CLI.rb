62a042a9f2fbe7377c5ece88ce818a0299170955
RochelLevi <rochlevi@gmail.com>
2017-11-16 14:01:20 -0500

    made ascii class

--- lib/app/CLI.rb
+++ lib/app/CLI.rb
@@ -1,40 +1,11 @@
 require_all 'lib'
 
 class CLI
-  def self.small_fish
-      puts <<-Art
-
-                                         _J""-.
-             .-""L_                     /o )   \ ,';
-        ;`, /   ( o\                    \ ,'    ;  /
-        \  ;    `, /                     "-.__.'"\_;
-        ;_/"`.__.-"
-
-        Art
-  end
-
-  def self.large_fish
-      puts <<-Art
-
-      ,-.           ,.---'''^\                  O
-     {   \       ,__\,---'''''`-.,      O    O
-      I   \    K`,'^           _  `'.     o
-      \  ,.J..-'`          // (O)   ,,X,    o
-      /  (_               ((   ~  ,;:''`  o
-     /   ,.X'.,            \\      ':;;;:
-    (_../      -._                  ,'`
-                K.=,;.__ /^~/___..'`
-                        /  /`
-                        ~~~
-
-        Art
-  end
-
 
   def self.welcome_list_choices
     puts "Hey Fish Head, we have alllll the fish in New York State. What can I get you?"
     puts "Input the number corresponding to the information you would like."
-    large_fish
+    ASCII.large_fish
   end
 
   def self.choices
@@ -42,25 +13,14 @@ class CLI
     puts "2 - Tell me all the species of fish available in a NY county"
     puts "3 - Tell me all the New York counties with a specific species of fish"
     puts "4 - Tell me all the bodies of water in a New York county"
+
     puts "5 - Exit"
   end
 
   def self.regreet
-    puts small_fish
+    ASCII.small_fish
     puts "Hey there! Quick reminder of your options"
   end
-
-  # def give_em_what_they_want(characteristic_name, to_know, type)
-  #   puts("Which #{characteristic_name} would you like to see #{to_know} for?")
-  #   input_name = gets.chomp.downcase
-  #   if !type.find_by(name: input_name)
-  #     puts("Hmmmm... Sorry, we don't have info on that #{characteristic_name}")
-  #   else
-  #     output = type.find_by(name: input_name).to_know.collect{|x|x[:name]}.to_sentence
-  #     puts("You can find #{output} in #{input_name}")
-  #   end
-  # end
-  #
 
 
   def self.input_1
