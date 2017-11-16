802faff016f0168281dc5e97d57dbb9a2bb75efe
chris <christofer.d.johnson@gmail.com>
2017-11-16 10:42:00 -0500

    Refactored Findrecipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ require 'rest-client'
 class User < ActiveRecord::Base
 	has_many :useringredients
 	has_many :ingredients, through: :useringredients
-
 	has_many :userrecipes
 	has_many :recipes, through: :userrecipes
 
 	def add_ingredients(array)
-		array.each do |x| 
+		array.each do |x|
+			x.downcase
 			ingredient = Ingredient.new(name: x)
 			ingredient.save
 			self.ingredients << ingredient
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
 	end
 
 	def findrecipes
+		binding.pry
 		ingredients = self.ingredients.map{|x| x.name}
 		
 		recipe_hash = {}
@@ -31,49 +32,65 @@ class User < ActiveRecord::Base
 			end
 		end
 
-		z = recipe_hash.keys.length
-		recipe_perm = [] 
-		until z < 2  do
-			recipe_perm << recipe_hash.keys.permutation(z).to_a.map!{|x| x.sort}.uniq
-			z -= 1
-		end
-		recipe_perm = recipe_perm.flatten(1)
+		# z = recipe_hash.keys.length
+		# recipe_perm = [] 
+		# until z < 2  do
+		# 	recipe_perm << recipe_hash.keys.permutation(z).to_a.map!{|x| x.sort}.uniq
+		# 	z -= 1
+		# end
+		# recipe_perm = recipe_perm.flatten(1)
 
 		recipe_hash.keys.each do |ingredient|
-			recipe_hash[ingredient] = recipe_hash[ingredient]['drinks'].map{|x| x['strDrink']}
+			recipe_hash[ingredient] = recipe_hash[ingredient]['drinks'].map{|x| x['idDrink']}
 		end
 
-		#recipe_hash
-		binding.pry
-		recipes = {}
-		recipe_perm.each do |array|
-			arr = []  
-			array.each do |x|   
-				arr << recipe_hash[x]       
-			end  
-			hsh = Hash.new(0)  
-			arr.flatten.collect{|x| hsh[x] += 1}
+		all_cocktails = Hash.new{|hash,key| hash[key]=[]}
 
-			recipes[array] = hsh.map{|k,v| 
-				if v == array.length 
-					k
-				end 
-			}
-		end 
-
-		recipes.each{|k,v| v.compact!}
-
-		recipes.each do |ingredients,recipe_names|
-			recipe_names.each do |recipe|
-				response = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{recipe}")
-				cocktail_ing = JSON.parse(response)
-				ingredient = cocktail_ing['drinks'][0].map{|k,v| v if k.include?("strIngredient")}.compact!.reject!(&:empty?)
-				
-				if ingredient && ingredient.length - ingredients.length >= 3 
-					recipe_names.delete(recipe)
-				end
+		recipe_hash.each do |ingredient,v|
+			recipe_hash[ingredient].each do |recipe_id|
+				all_cocktails[recipe_id] << ingredient
 			end
 		end
+
+
+		#recipe_hash
+		# binding.pry
+		# recipes = {}
+		# recipe_perm.each do |array|
+		# 	arr = []  
+		# 	array.each do |x|   
+		# 		arr << recipe_hash[x]       
+		# 	end  
+		# 	hsh = Hash.new(0)  
+		# 	arr.flatten.collect{|x| hsh[x] += 1}
+
+		# 	recipes[array] = hsh.map{|k,v| 
+		# 		if v == array.length 
+		# 			k
+		# 		end 
+		# 	}
+		# end 
+
+		# recipes.each{|k,v| v.compact!}
+
+		# recipes.each do |ingredients,recipe_names|
+		all_cocktails.each do |id, ingredients|
+			response = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}")
+			cocktail_ing = JSON.parse(response)
+			ingredient = cocktail_ing['drinks'][0].map{|ids,ingredient| ingredient.downcase if ingredient && ids.include?("strIngredient")}.compact!
+			ingredient.reject!(&:empty?)
+			all_cocktails[id] << ingredient
+			all_cocktails[id].flatten!.uniq!
+			#binding.pry
+		end
+
+		                 
+		 all_cocktails = Hash[all_cocktails.sort_by do |k,v|
+		 	duplicate = v 
+		     arr = duplicate - ingredients.to_a 
+		     arr.length
+		   end  
+		 ]  
 
 
 
